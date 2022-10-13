d5174b43d2c1e2efaed16692fd4d5d63357a08f7
yiderper <yider.per@gmail.com>
2022-10-12 21:53:11 -0500

    Formularios Reactive y Template en Angular

---
+++ src/app/reactive/basicos/basicos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent  {
+
+  constructor() { }
+
+}
